f1a881b8801493ec81297800a2c83836a5ae71ba
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-02-24 16:12:22 +0900

    feat : ArrayList 구현

---
+++ src/main/java/practice/SimpleArrayList.java
@@ -0,0 +1,108 @@
+package practice;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class SimpleArrayList implements SimpleList{
+
+    private int defaultCapacity = 10;
+    private String[] arrayList = new String[defaultCapacity];
+    private int endPointer = 0;
+
+    @Override
+    public boolean add(String value) {
+        grow();
+        arrayList[endPointer] = value;
+        endPointer++;
+        return true;
+    }
+
+    @Override
+    public void add(int index, String value) {
+        if (index > endPointer) {
+            throw new IndexOutOfBoundsException("범위를 넘었습니다");
+        }
+        endPointer += 1;
+        grow();
+        for (int i = index + 1; i < endPointer; i++) {
+            arrayList[i] = arrayList[i - 1];
+        }
+        arrayList[index] = value;
+    }
+
+    @Override
+    public String set(int index, String value) {
+        arrayList[index] = value;
+        return arrayList[index];
+    }
+
+    @Override
+    public String get(int index) {
+        return arrayList[index];
+    }
+
+    // Objects.equals(temp[i], value)
+    // .equals() 차이
+    @Override
+    public boolean contains(String value) {
+        for (int i = 0; i < endPointer; i++) {
+            if (Objects.equals(arrayList[i], value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int indexOf(String value) {
+        for (int i = 0; i < endPointer; i++) {
+            if (Objects.equals(arrayList[i], value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return endPointer;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return endPointer == 0;
+    }
+
+    @Override
+    public boolean remove(String value) {
+        int removeIndex = indexOf(value);
+        remove(removeIndex);
+        return true;
+    }
+
+    @Override
+    public String remove(int index) {
+        String removedValue = arrayList[index];
+        for (int i = index; i < endPointer - 1; i++) {
+            arrayList[i] = arrayList[i + 1];
+        }
+        endPointer -= 1;
+        return removedValue;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < endPointer; i++) {
+            arrayList[i] = null;
+        }
+        endPointer = 0;
+    }
+
+    private void grow() {
+        if (endPointer == arrayList.length) {
+            defaultCapacity += 100;
+            String[] copyTemp = Arrays.copyOf(arrayList, defaultCapacity);
+            arrayList = copyTemp;
+        }
+    }
+}
---
+++ src/test/java/ArrayListStudy.java
@@ -0,0 +1,78 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import practice.SimpleArrayList;
+import practice.SimpleList;
+
+public class ArrayListStudy {
+
+    @Test
+    public void arrayList() {
+        ArrayList<String> values = new ArrayList<>();
+        values.add("first");
+        values.add("second");
+
+
+        assertThat(values.add("third")).isTrue(); // 세 번째 값을 추가한다.
+        assertThat(values.size()).isEqualTo(3); // list의 크기를 구한다.
+        assertThat(values.get(0)).isEqualTo("first"); // 첫 번째 값을 찾는다.
+        assertThat(values.contains("first")).isTrue(); // "first" 값이 포함되어 있는지를 확인한다.
+        assertThat(values.remove(0)).isEqualTo("first"); // 첫 번째 값을 삭제한다.
+        assertThat(values.size()).isEqualTo(2); // 값이 삭제 됐는지 확인한다.
+
+        // TODO values에 담긴 모든 값을 출력한다.
+        for (int i = 0; i < values.size(); i++) {
+            System.out.println(values.get(i));
+        }
+    }
+
+    @Test
+    public void simpleList() {
+        SimpleList arrayList = new SimpleArrayList();
+        arrayList.add("first");
+        arrayList.add("second");
+        arrayList.add(2, "second2");
+
+        assertThat(arrayList.add("third")).isTrue(); // 세 번째 값을 추가한다..
+        assertThat(arrayList.size()).isEqualTo(4); // list의 크기를 구한다.
+        assertThat(arrayList.get(0)).isEqualTo("first");
+        assertThat(arrayList.set(0, "newFirst")).isEqualTo("newFirst");
+        assertThat(arrayList.contains("newFirst")).isTrue();
+        assertThat(arrayList.indexOf("newFirst")).isEqualTo(0);
+        assertThat(arrayList.indexOf("없음")).isEqualTo(-1);
+        assertThat(arrayList.remove("newFirst")).isTrue();
+        assertThat(arrayList.size()).isEqualTo(3);
+        assertThat(arrayList.remove(0)).isEqualTo("second");
+
+        arrayList.clear();
+        assertThat(arrayList.size()).isEqualTo(0);
+
+
+        // TODO values에 담긴 모든 값을 출력한다.
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+
+    @Test
+    public void growTest() {
+        SimpleList arrayList = new SimpleArrayList();
+
+        assertThatCode(() -> {
+            for (int i = 0; i < 100; i++) {
+                arrayList.add("temp");
+            }
+        }).doesNotThrowAnyException();
+        assertThat(arrayList.size()).isEqualTo(100);
+
+        arrayList.add(0, "new");
+        assertThat(arrayList.size()).isEqualTo(101);
+
+        // TODO values에 담긴 모든 값을 출력한다.
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+}
